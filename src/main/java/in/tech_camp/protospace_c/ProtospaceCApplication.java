a55026af93a134b6e20192c098e45d4dbd6e3db7
misa-beabl <misaki.inoue@smart-group.co.jp>
2025-06-16 11:37:52 +0900

    first commit

---
+++ src/main/java/in/tech_camp/protospace_c/ProtospaceCApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.protospace_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtospaceCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtospaceCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tech_camp/protospace_c/ProtospaceCApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.protospace_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtospaceCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
