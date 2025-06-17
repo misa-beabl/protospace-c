de434e6d4aab110b77d07a2123c6e0ac35ff678e
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-06-17 16:56:19 +0900

    Formの単体テストの実装

---
+++ src/test/java/in/tech_camp/protospace_c/PrototypeFactory.java
@@ -0,0 +1,26 @@
+package in.tech_camp.protospace_c;
+
+import org.springframework.mock.web.MockMultipartFile;
+
+import in.tech_camp.protospace_c.form.PrototypeForm;
+
+public class PrototypeFactory {
+  public static PrototypeForm createPrototypeForm() {
+    PrototypeForm prototypeForm = new PrototypeForm();
+
+    prototypeForm.setName("TestName");
+    prototypeForm.setSlogan("TestSlogan");
+    prototypeForm.setConcept("TestConcept");
+
+    // ダミーファイルを作成
+    MockMultipartFile mockImage = new MockMultipartFile(
+      "image",
+      "test.jpg",
+      "image/jpeg",
+      "fake image content".getBytes()
+    );
+    prototypeForm.setImage(mockImage);
+
+    return prototypeForm;
+  }
+}
