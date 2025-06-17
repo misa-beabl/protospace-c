6218604dab71896e9681510a9b1be9d9c541fe82
lyz-smart <liu.yuzhang@smart-group.co.jp>
2025-06-17 16:56:55 +0900

    commit for database settings

--- src/main/resources/db/migration/V2__create_prototypes_table.sql
+++ src/main/resources/db/migration/V2__create_prototypes_table.sql
@@ -1,10 +1,10 @@
 create TABLE prototypes (
   id serial primary KEY,
   user_id INT,
-  name VARCHAR(256),
-  slogan VARCHAR(512),
-  concept VARCHAR(512),
-  image VARCHAR(256),
+  name VARCHAR(256) not null,
+  slogan VARCHAR(512) not null,
+  concept VARCHAR(512) not null,
+  image VARCHAR(256) not null,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
