b3ba6c8e838abf6b0a83e08da0b77f4d79b5511d
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-06-17 13:32:43 +0900

    EntityとFormの作成

---
+++ src/main/java/in/tech_camp/protospace_c/form/PrototypeForm.java
@@ -0,0 +1,22 @@
+package in.tech_camp.protospace_c.form;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import in.tech_camp.protospace_c.validation.ValidationPriority1;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class PrototypeForm {
+  @NotBlank(message = "Name can't be blank",groups = ValidationPriority1.class)
+  private String name;
+
+  @NotBlank(message = "Slogan can't be blank",groups = ValidationPriority1.class)
+  private String slogan;
+
+  @NotBlank(message = "Concept can't be blank",groups = ValidationPriority1.class)
+  private String concept;
+
+  @NotBlank(message = "Image can't be blank",groups = ValidationPriority1.class)
+  private MultipartFile image;
+}
