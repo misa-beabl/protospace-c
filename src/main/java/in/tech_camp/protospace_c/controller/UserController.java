0ce5260be462ed87cb4191d2603ed381de1c5d59
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-06-18 15:17:37 +0900

    ユーザー詳細機能の実装

--- src/main/java/in/tech_camp/protospace_c/controller/UserController.java
+++ src/main/java/in/tech_camp/protospace_c/controller/UserController.java
@@ -10,11 +10,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import in.tech_camp.protospace_c.entity.PrototypeEntity;
 import in.tech_camp.protospace_c.entity.UserEntity;
 import in.tech_camp.protospace_c.form.UserForm;
+import in.tech_camp.protospace_c.repository.PrototypeRepository;
 import in.tech_camp.protospace_c.repository.UserRepository;
 import in.tech_camp.protospace_c.service.UserService;
 import in.tech_camp.protospace_c.validation.ValidationOrder;
@@ -24,6 +27,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class UserController {
   private final UserRepository userRepository;
+  private final PrototypeRepository prototypeRepository;
 
   private final UserService userService;
 
@@ -35,12 +39,9 @@ public class UserController {
 
   @PostMapping("/user")
   public String createUser(@ModelAttribute("userForm") @Validated(ValidationOrder.class) UserForm userForm, BindingResult result, Model model) {
-    // 确认密码与初始输入密码是不是一样，这种方法 validation 里边没有定义，所以定义在了表单类中
     userForm.validatePasswordConfirmation(result);
 
-    // 确认输入的邮箱是不是已经被注册，validation 之中一样没有定义，所以要先交互数据库确认是否存在
     if (userRepository.existsByEmail(userForm.getEmail())) {
-      // 如果邮箱已经被注册了，要向绑定结果中绑定错误信息
       result.rejectValue("email", "null", "Email already exists");
     }
 
@@ -72,7 +73,6 @@ public class UserController {
     return "redirect:/";
   }
 
-  // 登陆页面的 post 功能是由 spring security 自动实现的，在校验有问题的时候，会自动返回 error 的路径参数
   @GetMapping("/login")
   public String showLoginWithError(@RequestParam(value = "error") String error, Model model) {
     if (error != null) {
@@ -86,13 +86,13 @@ public class UserController {
       return "users/login";
   }
 
-  // @GetMapping("/users/{userId}")
-  // public String showMypage(@PathVariable("userId") Integer userId, Model model) {
-  //   UserEntity user = userRepository.findById(userId);
-  //   List<PrototypeEntity> prototypes = user.getPrototypes();
+  @GetMapping("/users/{userId}")
+  public String showMypage(@PathVariable("userId") Integer userId, Model model) {
+    UserEntity user = userRepository.findById(userId);
+    List<PrototypeEntity> prototypes = prototypeRepository.findByUserId(userId);
 
-  //   model.addAttribute("nickname", user.getNickname());
-  //   model.addAttribute("prototypes", prototypes);
-  //   return "users/mypage";
-  // }
+    model.addAttribute("user", user);
+    model.addAttribute("prototypes", prototypes);
+    return "users/detail";
+  }
 }
